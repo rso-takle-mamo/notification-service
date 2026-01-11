9c842a4c58ad7e7b6fba2da22149417baaba1458
Maks Klemenčič <maks.klemencic6@gmail.com>
2026-01-11 23:09:01 +0100

    adding cors

---
+++ src/NotificationService.Api/Configuration/CorsSettings.cs
@@ -0,0 +1,12 @@
+namespace NotificationService.Api.Configuration;
+
+public class CorsSettings
+{
+    public const string SectionName = "Cors";
+
+    public string[] AllowedOrigins { get; set; } = [
+        "http://localhost:3000",
+        "https://appointments-booking-rso-dev.vercel.app",
+        "https://appointments-booking-rso.vercel.app"
+    ];
+}
